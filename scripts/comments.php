f144b60b8dff340e4b849946fc7fae23d3a91fba
Siigismund <lahdenperamiika@gmail.com>
2023-02-24 13:17:22 +0200

    Delete comment function

--- scripts/comments.php
+++ scripts/comments.php
@@ -24,8 +24,13 @@ function getComment($con){
         echo $row['comment_time']."<br>";
         echo nl2br($row['comment_text'])."<br><br>";
 
+        //Edit and delete buttons
         echo 
-            "<form method='POST' action='editcomment.php'>
+            "<form method='POST' action='".deleteComment($con)."'>
+            <input type='hidden' name='id' value='".$row['id']."'>
+            <button name='commentDelete'>Delete</button>
+            </form>
+            <form method='POST' action='editcomment.php'>
                 <input type='hidden' name='id' value='".$row['id']."'>
                 <input type='hidden' name='user_id' value='".$row['user_id']."'>
                 <input type='hidden' name='comment_time' value='".$row['comment_time']."'>
@@ -35,6 +40,7 @@ function getComment($con){
     }
 }
 
+//Edit function
 function editComment($con){
     if (isset($_POST['editSubmit'])){
         $id = $_POST['id'];
@@ -43,6 +49,18 @@ function editComment($con){
         $comment = $_POST['comment_text'];
 
         $sql = "UPDATE comment SET comment_text='$comment' WHERE id='$id'";
+
+        $result = $con->query($sql);
+        exit(header("location: index.php"));
+    }
+}
+
+//Delete function
+function deleteComment($con){
+    if (isset($_POST['commentDelete'])){
+        $id = $_POST['id'];
+
+        $sql = "DELETE FROM comment WHERE id='$id'";
 
         $result = $con->query($sql);
         exit(header("location: index.php"));

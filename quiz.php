84d482638670cd984123da52678c1c88b7ff10f7
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-02-28 18:46:51 +0200

    button style

---
+++ quiz.php
@@ -0,0 +1,7 @@
+<?php 
+include 'layout/header.php';
+?>
+<h2>Do you know your club well?</h2>
+<?php
+include 'layout/footer.php';
+?>
--- tables.php
+++ tables.php
@@ -3,19 +3,24 @@ include "layout/header.php";
 
 ?>
 <div class="tables" style=" width: 70%; margin: auto;">
-    <h2></h2>
 
-    <form method="post">
-        <select name="competitions">
-            <option value="all">All Competitions</option>
-            <option value="epl">English Premier League</option>
-            <option value="ucl">UEFA Champions League</option>
-        </select>
-        <select name="season">
-            <option value="s23">2022-23</option>
-            <option value="s22">2021-22</option>
-        </select>
-        <input type="submit" value="Submit" name="submit">
+    <form method="post" class="row g-3">
+        <div class="col-auto">
+            <select name="competitions" class="form-select">
+                <option value="all">All Competitions</option>
+                <option value="epl">English Premier League</option>
+                <option value="ucl">UEFA Champions League</option>
+            </select>
+        </div>
+        <div class="col-auto">
+            <select name="season" class="form-select">
+                <option value="s23">2022-23</option>
+                <option value="s22">2021-22</option>
+            </select>
+        </div>
+        <div class="col-auto">
+            <input type="submit" class="btn btn-outline-danger" value="SHOW" name="SHOW">
+        </div>
     </form>
 
 
@@ -23,7 +28,7 @@ include "layout/header.php";
     $epl22_23 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUQRlKDVZfBpigOrtJaCX1K05ySMjJe16LGGlmdyG5BhBa2d5mY1J9KByl10utiJFszJILYyBEDgLt/pub?gid=0&single=true&output=csv";
     $ucl22_23 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUQRlKDVZfBpigOrtJaCX1K05ySMjJe16LGGlmdyG5BhBa2d5mY1J9KByl10utiJFszJILYyBEDgLt/pub?gid=232218589&single=true&output=csv";
 
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['SHOW'])) {
         $competition = $_POST['competitions'];
         $season = $_POST['season'];
 
@@ -34,52 +39,50 @@ include "layout/header.php";
 
             printLiveTable($epl22_23, 'epl22_23');
 
-        }
-        else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s23") === 0) {
-    
+        } else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s23") === 0) {
+
             echo "<h1>UEFA Champions League Season 2022/23</h1>";
             printLiveTable($ucl22_23, 'ucl22_23');
-    
+
         } else if (strcmp($competition, "all") === 0 and strcmp($season, "s23") === 0) {
-    
+
             echo "<h1>English Premier League Season 2022/23</h1>";
-    
+
             printLiveTable($epl22_23, 'ucl22_23');
-    
+
             echo "<h1>UEFA Champions League Season 2022/23</h1>";
-    
+
             printLiveTable($ucl22_23, 'ucl22_23');
-    
+
         }
         // Displaying data for the previous season 
-        else   if (strcmp($competition, "epl") === 0 and strcmp($season, "s22") === 0) {
+        else if (strcmp($competition, "epl") === 0 and strcmp($season, "s22") === 0) {
 
             echo "<h1>English Premier League Season 2021/22</h1>";
-    
+
             printTable('epl21_22');
 
-        }
-        else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s22") === 0) {
-    
+        } else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s22") === 0) {
+
             echo "<h1>UEFA Champions League Season 2021/22</h1>";
-    
+
             printTable('ucl21_22');
-    
+
         } else if (strcmp($competition, "all") === 0 and strcmp($season, "s22") === 0) {
-    
+
             echo "<h1>English Premier League Season 2021/22</h1>";
-    
+
             printTable('epl21_22');
-    
+
             echo "<h1>UEFA Champions League Season 2021/22</h1>";
-    
+
             printTable('ucl21_22');
-    
+
         }
 
-    }else{
+    } else {
         echo "<h1>English Premier League Season 2022/23</h1>";
-    
+
         printLiveTable($epl22_23, 'ucl22_23');
 
         echo "<h1>UEFA Champions League Season 2022/23</h1>";
@@ -151,7 +154,8 @@ include "layout/header.php";
     }
 
     // Printing the data for the previous competitons 
-    function printTable($table){
+    function printTable($table)
+    {
 
         include 'edvin_db.php';
         echo "<table class=\"table\">
@@ -167,13 +171,14 @@ include "layout/header.php";
        <th>GD</th>
        <th>Pts</th>
       </tr>";
-      printData($table, $conn);
+        printData($table, $conn);
 
-      $conn->close();
+        $conn->close();
     }
 
     // Printing the data from database 
-    function printData($table, $conn){
+    function printData($table, $conn)
+    {
         // Getting data from the database and printing it out  
         $read = "SELECT * FROM `" . $table . "`";
         $result = $conn->query($read);

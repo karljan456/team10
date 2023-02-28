937ad4a7159262f512094666d35421acc2b55719
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-02-28 20:03:15 +0200

    competition icons

--- tables.php
+++ tables.php
@@ -22,12 +22,9 @@ include "layout/header.php";
             <input type="submit" class="btn btn-outline-danger" value="SHOW" name="SHOW">
         </div>
     </form>
-
-
     <?php
     $epl22_23 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUQRlKDVZfBpigOrtJaCX1K05ySMjJe16LGGlmdyG5BhBa2d5mY1J9KByl10utiJFszJILYyBEDgLt/pub?gid=0&single=true&output=csv";
     $ucl22_23 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUQRlKDVZfBpigOrtJaCX1K05ySMjJe16LGGlmdyG5BhBa2d5mY1J9KByl10utiJFszJILYyBEDgLt/pub?gid=232218589&single=true&output=csv";
-
     if (isset($_POST['SHOW'])) {
         $competition = $_POST['competitions'];
         $season = $_POST['season'];
@@ -35,21 +32,25 @@ include "layout/header.php";
         // Displaying data for the current season 
         if (strcmp($competition, "epl") === 0 and strcmp($season, "s23") === 0) {
 
+            
+            echo "<img src=\"assets\images\\epl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>English Premier League Season 2022/23</h1>";
-
             printLiveTable($epl22_23, 'epl22_23');
 
         } else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s23") === 0) {
 
+            echo "<img src=\"assets\images\\ucl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>UEFA Champions League Season 2022/23</h1>";
             printLiveTable($ucl22_23, 'ucl22_23');
 
         } else if (strcmp($competition, "all") === 0 and strcmp($season, "s23") === 0) {
 
+            echo "<img src=\"assets\images\\epl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>English Premier League Season 2022/23</h1>";
 
             printLiveTable($epl22_23, 'ucl22_23');
 
+            echo "<img src=\"assets\images\\ucl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>UEFA Champions League Season 2022/23</h1>";
 
             printLiveTable($ucl22_23, 'ucl22_23');
@@ -58,22 +59,26 @@ include "layout/header.php";
         // Displaying data for the previous season 
         else if (strcmp($competition, "epl") === 0 and strcmp($season, "s22") === 0) {
 
+            echo "<img src=\"assets\images\\epl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>English Premier League Season 2021/22</h1>";
 
             printTable('epl21_22');
 
         } else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s22") === 0) {
 
+            echo "<img src=\"assets\images\\ucl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>UEFA Champions League Season 2021/22</h1>";
 
             printTable('ucl21_22');
 
         } else if (strcmp($competition, "all") === 0 and strcmp($season, "s22") === 0) {
 
+            echo "<img src=\"assets\images\\epl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>English Premier League Season 2021/22</h1>";
 
             printTable('epl21_22');
 
+            echo "<img src=\"assets\images\\ucl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
             echo "<h1>UEFA Champions League Season 2021/22</h1>";
 
             printTable('ucl21_22');
@@ -81,10 +86,12 @@ include "layout/header.php";
         }
 
     } else {
+        echo "<img src=\"assets\images\\epl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
         echo "<h1>English Premier League Season 2022/23</h1>";
 
         printLiveTable($epl22_23, 'ucl22_23');
 
+        echo "<img src=\"assets\images\\ucl_logo.png\" alt=\"epl logo\" align=\"right\" width=\"100\" height=\"70\">";
         echo "<h1>UEFA Champions League Season 2022/23</h1>";
 
         printLiveTable($ucl22_23, 'ucl22_23');
@@ -186,17 +193,18 @@ include "layout/header.php";
             while ($row = $result->fetch_assoc()) {
                 if (strcmp($row['Team'], "Liverpool") === 0) {
                     echo "<tr>
-                    <td><b>$row[Pos]</b></td>
-                    <td><b>$row[Team]</b></td>
-                    <td><b>$row[Pld]</b></td>
-                    <td><b>$row[W]</b></td>
-                    <td><b>$row[D]</b></td>
-                    <td><b>$row[L]</b></td>
-                    <td><b>$row[GF]</b></td>
-                    <td><b>$row[GA]</b></td>
-                    <td><b>$row[GD]</b></td>
-                    <td><b>$row[Pts]</b></td>
-                   </tr>";
+                        <td><b>$row[Pos]</b></td>
+                        <td><b>$row[Team]</b></td>
+                        <td><b>$row[Pld]</b></td>
+                        <td><b>$row[W]</b></td>
+                        <td><b>$row[D]</b></td>
+                        <td><b>$row[L]</b></td>
+                        <td><b>$row[GF]</b></td>
+                        <td><b>$row[GA]</b></td>
+                        <td><b>$row[GD]</b></td>
+                        <td><b>$row[Pts]</b></td>
+                    </tr>
+                </tbody>";
                 } else {
                     echo "<tr>
                     <td>$row[Pos]</td>

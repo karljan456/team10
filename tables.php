71339cab4bc6db1c6c7e105e7aa479a9deb66645
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-02-28 17:06:28 +0200

    league table functionality is done

--- tables.php
+++ tables.php
@@ -2,7 +2,7 @@
 include "layout/header.php";
 
 ?>
-<div style=" width: 70%; margin: auto;">
+<div class="tables" style=" width: 70%; margin: auto;">
     <h2></h2>
 
     <form method="post">
@@ -27,35 +27,69 @@ include "layout/header.php";
         $competition = $_POST['competitions'];
         $season = $_POST['season'];
 
+        // Displaying data for the current season 
         if (strcmp($competition, "epl") === 0 and strcmp($season, "s23") === 0) {
 
             echo "<h1>English Premier League Season 2022/23</h1>";
 
-            printTable($epl22_23);
+            printLiveTable($epl22_23, 'epl22_23');
 
-        } else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s23") === 0) {
-
-            echo "<h1>UEFA Champions League Season 2022/23</h1>";
-            printTable($ucl22_23);
-
-        } else if (strcmp($competition, "all") === 0 and strcmp($season, "s23") === 0) {
-
-            echo "<h1>English Premier League Season 2022/23</h1>";
-
-            printTable($epl22_23);
-
-            echo "<h1>UEFA Champions League Season 2022/23</h1>";
-
-            printTable($ucl22_23);
-
-        } else {
-            echo 'Will be added soon...';
         }
+        else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s23") === 0) {
+    
+            echo "<h1>UEFA Champions League Season 2022/23</h1>";
+            printLiveTable($ucl22_23, 'ucl22_23');
+    
+        } else if (strcmp($competition, "all") === 0 and strcmp($season, "s23") === 0) {
+    
+            echo "<h1>English Premier League Season 2022/23</h1>";
+    
+            printLiveTable($epl22_23, 'ucl22_23');
+    
+            echo "<h1>UEFA Champions League Season 2022/23</h1>";
+    
+            printLiveTable($ucl22_23, 'ucl22_23');
+    
+        }
+        // Displaying data for the previous season 
+        else   if (strcmp($competition, "epl") === 0 and strcmp($season, "s22") === 0) {
+
+            echo "<h1>English Premier League Season 2021/22</h1>";
+    
+            printTable('epl21_22');
+
+        }
+        else if (strcmp($competition, "ucl") === 0 and strcmp($season, "s22") === 0) {
+    
+            echo "<h1>UEFA Champions League Season 2021/22</h1>";
+    
+            printTable('ucl21_22');
+    
+        } else if (strcmp($competition, "all") === 0 and strcmp($season, "s22") === 0) {
+    
+            echo "<h1>English Premier League Season 2021/22</h1>";
+    
+            printTable('epl21_22');
+    
+            echo "<h1>UEFA Champions League Season 2021/22</h1>";
+    
+            printTable('ucl21_22');
+    
+        }
+
+    }else{
+        echo "<h1>English Premier League Season 2022/23</h1>";
+    
+        printLiveTable($epl22_23, 'ucl22_23');
+
+        echo "<h1>UEFA Champions League Season 2022/23</h1>";
+
+        printLiveTable($ucl22_23, 'ucl22_23');
 
     }
 
 
-    function printTable($url)
+    function printLiveTable($url, $table)
     {
         echo "<table class=\"table\">
         <tr>
@@ -72,47 +106,40 @@ include "layout/header.php";
       </tr>";
         $data = getData($url);
         $i = 0;
+        // Going through the array with data 
         for ($i = 1; $i < count($data); $i++) {
 
             list($Pos, $Team, $Pld, $W, $D, $L, $GF, $GA, $GD, $Pts) = $data[$i];
 
-            if (strcmp($Team, "Liverpool") === 0) {
-                echo "<tr>
-            <td><b>$Pos</b></td>
-            <td><b>$Team</b></td>
-            <td><b>$Pld</b></td>
-            <td><b>$W</b></td>
-            <td><b>$D</b></td>
-            <td><b>$L</b></td>
-            <td><b>$GF</b></td>
-            <td><b>$GA</b></td>
-            <td><b>$GD</b></td>
-            <td><b>$Pts</b></td>
-           </tr>";
-            } else {
-                echo "<tr>
-            <td>$Pos</td>
-            <td>$Team</td>
-            <td>$Pld</td>
-            <td>$W</td>
-            <td>$D</td>
-            <td>$L</td>
-            <td>$GF</td>
-            <td>$GA</td>
-            <td>$GD</td>
-            <td>$Pts</td>
-           </tr>";
-            }
+            include 'edvin_db.php';
 
+            // Putting the data into the database
+            $insert = "INSERT INTO `" . $table . "` (`Pos`, `Team`, `Pld`, `W`, `D`, `L`, `GF`, `GA`, `GD`, `Pts`) VALUES ('$Pos', '$Team',
+             '$Pld', '$W', '$D', '$L', '$GF', '$GA', '$GD', '$Pts')";
+
+            if ($conn->query($insert) === TRUE) {
+                // echo 'Data added!';
+            } else {
+                // die($conn->connect_error);
+            }
         }
-        echo "</table>";
+
+        printData($table, $conn);
+
+        // Deleting data so it will be renewed with the newer one when it is available
+        $delete = "DELETE FROM tables." . $table . "";
+
+        mysqli_query($conn, $delete);
+
+        $conn->close();
     }
 
+    // reading online CSV file from the server
     function getData($url)
     {
         $array = [];
         if (($handle = fopen($url, "r")) !== false) {
-            while (($data = fgetcsv($handle, 92, ",")) !== false) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
 
                 $array[] = $data;
 
@@ -121,6 +148,74 @@ include "layout/header.php";
             return $array;
         } else
             die("Problem reading csv");
+    }
+
+    // Printing the data for the previous competitons 
+    function printTable($table){
+
+        include 'edvin_db.php';
+        echo "<table class=\"table\">
+        <tr>
+       <th>POSTION</th>
+       <th>TEAM</th>
+       <th>PLAYED</th>
+       <th>WON</th>
+       <th>DRAWN</th>
+       <th>LOST</th>
+       <th>GF</th>
+       <th>GA</th>
+       <th>GD</th>
+       <th>Pts</th>
+      </tr>";
+      printData($table, $conn);
+
+      $conn->close();
+    }
+
+    // Printing the data from database 
+    function printData($table, $conn){
+        // Getting data from the database and printing it out  
+        $read = "SELECT * FROM `" . $table . "`";
+        $result = $conn->query($read);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                if (strcmp($row['Team'], "Liverpool") === 0) {
+                    echo "<tr>
+                    <td><b>$row[Pos]</b></td>
+                    <td><b>$row[Team]</b></td>
+                    <td><b>$row[Pld]</b></td>
+                    <td><b>$row[W]</b></td>
+                    <td><b>$row[D]</b></td>
+                    <td><b>$row[L]</b></td>
+                    <td><b>$row[GF]</b></td>
+                    <td><b>$row[GA]</b></td>
+                    <td><b>$row[GD]</b></td>
+                    <td><b>$row[Pts]</b></td>
+                   </tr>";
+                } else {
+                    echo "<tr>
+                    <td>$row[Pos]</td>
+                    <td>$row[Team]</td>
+                    <td>$row[Pld]</td>
+                    <td>$row[W]</td>
+                    <td>$row[D]</td>
+                    <td>$row[L]</td>
+                    <td>$row[GF]</td>
+                    <td>$row[GA]</td>
+                    <td>$row[GD]</td>
+                    <td>$row[Pts]</td>
+                   </tr>";
+                }
+            }
+
+            echo "</table>";
+
+        } else {
+
+            echo "No data to display";
+
+
+        }
     }
 
     ?>

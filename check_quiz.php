e47e8b7dc423182221ef1637737fcbf833bec8f1
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-03-02 20:02:42 +0200

    quiz functionality is completed

---
+++ check_quiz.php
@@ -0,0 +1,52 @@
+<?php
+include 'layout/header.php';
+?>
+
+<div style="width:20%; margin: auto;">
+
+<?php
+
+include 'edvin_db.php';
+
+if (isset($_POST['submit'])) {
+
+
+    $points = 0;
+    $total_questions = 5; 
+    $user_answers = array();
+
+    // Looping through every question in the quiz
+    for ($i = 1; $i <= $total_questions; $i++) {
+        // Retrieving the user answer for this question from $_POST
+        $user_answer = $_POST['answer' . $i];
+        $user_answers[$i] = $user_answer;
+
+        $check = "SELECT correct_answer FROM `quiz` WHERE id = $i";
+
+        // Executing the query and retrieving the result set
+        $result = $conn->query($check);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $correct_answer = $row['correct_answer'];
+
+            // Check if the user answer is correct and increase the score if it is
+            if ($user_answer == $correct_answer) {
+                $points++;
+            }
+        }
+    }
+
+    echo "<h2>Your score: $points/" . $total_questions . "</h2>";
+
+    $conn->close();
+
+}
+
+?>
+
+</div>
+
+<?php
+include 'layout/footer.php';
+?>
--- quiz.php
+++ quiz.php
@@ -12,17 +12,18 @@ include 'layout/header.php';
     $result = $conn->query($get_data);
     if ($result->num_rows > 0) {
 
-        echo '<form method="post" class="row g-3 align-items-center">';
+        echo '<form method="post" action="check_quiz.php" class="row g-3 align-items-center">';
 
         while ($row = $result->fetch_assoc()) {
 
             echo "<h2>$row[question]</h2>
         <img src='assets\images\quiz\\$row[id].jpg' alt='$row[correct_answer]' width='70%' height='50%'>
         <div class=\"col-auto\">
-    <input type=\"radio\" class=\"form-check-input\" name=\"answer\" value=\"$row[option_1]\">$row[option_1]<br>
-    <input type=\"radio\" class=\"form-check-input\" name=\"answer\" value=\"$row[option_2]\">$row[option_2]<br>
-    <input type=\"radio\" class=\"form-check-input\" name=\"answer\" value=\"$row[option_3]\">$row[option_3]<br>
-    <input type=\"radio\" class=\"form-check-input\" name=\"answer\" value=\"$row[option_4]\">$row[option_4]<br>
+        <input type=\"hidden\" name=\"question$row[id]\" value=\"$row[id]\">
+    <input type=\"radio\" class=\"form-check-input\" name=\"answer$row[id]\" value=\"$row[option_1]\">$row[option_1]<br>
+    <input type=\"radio\" class=\"form-check-input\" name=\"answer$row[id]\" value=\"$row[option_2]\">$row[option_2]<br>
+    <input type=\"radio\" class=\"form-check-input\" name=\"answer$row[id]\" value=\"$row[option_3]\">$row[option_3]<br>
+    <input type=\"radio\" class=\"form-check-input\" name=\"answer$row[id]\" value=\"$row[option_4]\">$row[option_4]<br>
 </div>
 ";
 
@@ -33,38 +34,6 @@ include 'layout/header.php';
 
     }
 
-
-    ?>
-
-    <?php
-
-    include 'edvin_db.php';
-
-    if (isset($_POST['submit'])) {
-
-        $points = 0;
-
-        $check = "SELECT * FROM `quiz`";
-
-        $result = $conn->query($check);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-
-                $user_answer = $_POST['answer'];
-
-                if ($user_answer == $row['correct_answer']) {
-                    $points++;
-                }
-            }
-        }
-
-
-        echo "<h1>Your score: $points/" . $result->num_rows . "</h1>";
-        
-        $conn-> close();
-    
-    }
 
     ?>
 </div>

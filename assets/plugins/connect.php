8bc2cf245f2a13a85ed21a93276aa3bd9c65899f
El-SaMa <113467305+El-SaMa@users.noreply.github.com>
2023-03-07 19:15:53 +0200

    ..

--- assets/plugins/connect.php
+++ assets/plugins/connect.php
@@ -1,10 +1,14 @@
 <?php
 
-$hostname = "db";
-$db_name = "team10_lfc";
-$db_user= "team10_lfc";
-$db_password = "wCZF*Pw[=gML";
+//$hostname = "db";
+//$db_name = "team10_lfc";
+//$db_user= "team10_lfc";
+//$db_password = "wCZF*Pw[=gML";
 
+$hostname = "localhost";
+$db_name = "wp_bbcap22_10";
+$db_user= "bbcap22_10";
+$db_password = "b0R5EHMX";
 
 
 $con = new mysqli($hostname, $db_user, $db_password, $db_name);

24356b883ba24529199b5865f1d39f7cf10864f0
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-03-07 17:27:11 +0200

    edvin features db

--- assets/plugins/edvin_tables_quiz_db.sql
+++ assets/plugins/edvin_tables_quiz_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Mar 01, 2023 at 11:27 AM
+-- Generation Time: Mar 07, 2023 at 03:25 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.0.19
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tables`
+-- Database: `team10_lfc`
 --
 
 -- --------------------------------------------------------
@@ -46,7 +46,7 @@ CREATE TABLE `epl21_22` (
 --
 
 INSERT INTO `epl21_22` (`id`, `Pos`, `Team`, `Pld`, `W`, `D`, `L`, `GF`, `GA`, `GD`, `Pts`) VALUES
-(1, 1, 'Manchester City (C)', 38, 29, 6, 3, 99, 26, '+73', '93'),
+(1, 1, 'Manchester City', 38, 29, 6, 3, 99, 26, '+73', '93'),
 (2, 2, 'Liverpool', 38, 28, 8, 2, 94, 26, '+68', '92'),
 (3, 3, 'Chelsea', 38, 21, 11, 6, 76, 33, '+43', '74'),
 (4, 4, 'Tottenham Hotspur', 38, 22, 5, 11, 69, 40, '+29', '71'),
@@ -63,9 +63,9 @@ INSERT INTO `epl21_22` (`id`, `Pos`, `Team`, `Pld`, `W`, `D`, `L`, `GF`, `GA`, `
 (15, 15, 'Southampton', 38, 9, 13, 16, 43, 67, '−24', '40'),
 (16, 16, 'Everton', 38, 11, 6, 21, 43, 66, '−23', '39'),
 (17, 17, 'Leeds United', 38, 9, 11, 18, 42, 79, '−37', '38'),
-(18, 18, 'Burnley (R)', 38, 7, 14, 17, 34, 53, '−19', '35'),
-(19, 19, 'Watford (R)', 38, 6, 5, 27, 34, 77, '−43', '23'),
-(20, 20, 'Norwich City (R)', 38, 5, 7, 26, 23, 84, '−61', '22');
+(18, 18, 'Burnley', 38, 7, 14, 17, 34, 53, '−19', '35'),
+(19, 19, 'Watford', 38, 6, 5, 27, 34, 77, '−43', '23'),
+(20, 20, 'Norwich City', 38, 5, 7, 26, 23, 84, '−61', '22');
 
 -- --------------------------------------------------------
 
@@ -86,6 +86,64 @@ CREATE TABLE `epl22_23` (
   `GD` varchar(10) DEFAULT NULL,
   `Pts` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `epl22_23`
+--
+
+INSERT INTO `epl22_23` (`id`, `Pos`, `Team`, `Pld`, `W`, `D`, `L`, `GF`, `GA`, `GD`, `Pts`) VALUES
+(741, 1, 'Arsenal', 26, 20, 3, 3, 59, 25, '+34', '63'),
+(742, 2, 'Manchester City', 26, 18, 4, 4, 66, 25, '+41', '58'),
+(743, 3, 'Manchester United', 25, 15, 4, 6, 41, 35, '+6', '49'),
+(744, 4, 'Tottenham Hotspur', 26, 14, 3, 9, 46, 36, '+10', '45'),
+(745, 5, 'Liverpool', 25, 12, 6, 7, 47, 28, '+19', '42'),
+(746, 6, 'Newcastle United', 24, 10, 11, 3, 35, 17, '+18', '41'),
+(747, 7, 'Fulham', 26, 11, 6, 9, 38, 34, '+4', '39'),
+(748, 8, 'Brighton & Hove Albion', 23, 11, 5, 7, 43, 29, '+14', '38'),
+(749, 9, 'Brentford', 24, 9, 11, 4, 40, 32, '+8', '38'),
+(750, 10, 'Chelsea', 25, 9, 7, 9, 24, 25, '-1', '34'),
+(751, 11, 'Aston Villa', 25, 10, 4, 11, 31, 38, '-7', '34'),
+(752, 12, 'Crystal Palace', 25, 6, 9, 10, 21, 32, '-11', '27'),
+(753, 13, 'Wolverhampton Wanderers', 26, 7, 6, 13, 19, 35, '-16', '27'),
+(754, 14, 'Nottingham Forest', 25, 6, 8, 11, 20, 44, '-24', '26'),
+(755, 15, 'Leicester City', 25, 7, 3, 15, 36, 43, '-7', '24'),
+(756, 16, 'West Ham United', 25, 6, 5, 14, 23, 33, '-10', '23'),
+(757, 17, 'Leeds United', 25, 5, 7, 13, 29, 40, '-11', '22'),
+(758, 18, 'Everton', 26, 5, 7, 14, 19, 38, '-19', '22'),
+(759, 19, 'Southampton', 25, 6, 3, 16, 20, 41, '-21', '21'),
+(760, 20, 'Bournemouth', 25, 5, 6, 14, 24, 51, '-27', '21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id` int NOT NULL,
+  `question` varchar(60) DEFAULT NULL,
+  `option_1` varchar(40) DEFAULT NULL,
+  `option_2` varchar(40) DEFAULT NULL,
+  `option_3` varchar(40) DEFAULT NULL,
+  `option_4` varchar(40) DEFAULT NULL,
+  `correct_answer` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`id`, `question`, `option_1`, `option_2`, `option_3`, `option_4`, `correct_answer`) VALUES
+(1, 'When was Liverpool FC founded?', '1914', '1892', 'Last year', '2000', '1892'),
+(2, 'What is the name of the bird on the Liverpool badge?', 'Magpie', 'Eagle', 'Liver Bird', 'Comorant', 'Liver Bird'),
+(3, 'What are the terraces at Anfield also known as?', 'The Kip', 'The Kop', 'The Kup', 'The Kap', 'The Kop'),
+(4, 'Where is Mohamed Salah from?', 'Norway', 'Finland', 'United States', 'Egypt', 'Egypt'),
+(5, 'Which of these people HAS NEVER played for Liverpool?', 'Steven Gerrard', 'Mario Balotelli', 'Zlatan Ibrahimovic', 'Xabi Alonso', 'Zlatan Ibrahimovic'),
+(6, 'Which LFC player won the Ballon D\'or in 2001?', 'Steven Gerrard', 'Jamie Carragher', 'Sami Hyypiä', 'Michael Owen', 'Michael Owen'),
+(7, 'When did Liverpool win their first FA Cup?', '1965', '2022', '1981', '2001', '1965'),
+(8, 'Which newspaper is boycotted in Liverpool?', 'The Daily Mail', 'The Times', 'The Sun', 'The Guardian', 'The Sun'),
+(9, 'What is the name of Liverpool FC home ground?', 'Old Trafford', 'Goodison Park', 'Anfield', 'Wembley', 'Anfield'),
+(10, 'What’s the name of the two liver birds by the docks?', 'Bella & Bertie', 'Bertha & Bernie', 'Bernie & Bianca', 'Bernard & Isabella', 'Bella & Bertie');
 
 -- --------------------------------------------------------
 
@@ -138,6 +196,16 @@ CREATE TABLE `ucl22_23` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
+-- Dumping data for table `ucl22_23`
+--
+
+INSERT INTO `ucl22_23` (`id`, `Pos`, `Team`, `Pld`, `W`, `D`, `L`, `GF`, `GA`, `GD`, `Pts`) VALUES
+(609, 1, 'Napoli', 6, 5, 0, 1, 20, 6, '+14', '15'),
+(610, 2, 'Liverpool', 6, 5, 0, 1, 17, 6, '+11', '15'),
+(611, 3, 'Ajax', 6, 2, 0, 4, 11, 16, '-5', '6'),
+(612, 4, 'Rangers', 6, 0, 0, 6, 2, 22, '-20', '0');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -151,6 +219,12 @@ ALTER TABLE `epl21_22`
 -- Indexes for table `epl22_23`
 --
 ALTER TABLE `epl22_23`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -179,7 +253,13 @@ ALTER TABLE `epl21_22`
 -- AUTO_INCREMENT for table `epl22_23`
 --
 ALTER TABLE `epl22_23`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=701;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=761;
+
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `ucl21_22`
@@ -191,7 +271,7 @@ ALTER TABLE `ucl21_22`
 -- AUTO_INCREMENT for table `ucl22_23`
 --
 ALTER TABLE `ucl22_23`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=601;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=613;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
